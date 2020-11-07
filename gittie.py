19b7d6c3da428210196cfb175b0d87d52564e3c4
Marianna Krawczak <mariannakrawczak@o2.pl>
2020-11-07 16:32:08 +0100

    Able to add input

--- gittie.py
+++ gittie.py
@@ -1,2 +1,10 @@
 def introduce() -> None:
     print("Hello, I'm Gittie!")
+def add():
+    added = input ("Add your input: ")
+    print (added)
+
+
+if __name__ == "__main__":
+    introduce()
+    add()
